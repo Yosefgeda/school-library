f07718873638074af192dc1f4510df72dc10b988
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-05-23 18:41:58 +0300

    Decorate structure

--- person.rb
+++ person.rb
@@ -1,5 +1,8 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -11,6 +14,10 @@ class Person
     of_age? || @parent_permission == true
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
@@ -19,3 +26,39 @@ class Person
     false
   end
 end
+
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.upcase
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.length > 10
+      @nameable.correct_name.slice(0, 10)
+    else
+      @nameable.correct_name
+    end
+  end
+end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name # maximilianus
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name # MAXIMILIANUS
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name # MAXIMILIAN
